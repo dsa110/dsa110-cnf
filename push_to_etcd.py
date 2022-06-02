6cfd8afa7b09e5d2aa5a45849b6f82dd9938f53c
ubuntu <ubuntu@pm.me>
2022-06-02 21:43:46 +0000

    push_to_etcd also can print values if no file provided

--- push_to_etcd.py
+++ push_to_etcd.py
@@ -3,22 +3,32 @@ import yaml
 import dsautils.dsa_store as ds
 import sys
 
-# first argument is key, second argument is file
-key = sys.argv[1]
-fl = sys.argv[2]
-
-# read file
-with open(fl, 'r') as stream:
-    try:
-        dct = yaml.load(stream, Loader=yaml.FullLoader)
-    except yaml.YAMLError as exc:
-        print('cannot open yaml file')
-        exit(1)
-
 # open etcd connection
 my_ds = ds.DsaStore()
 
-# push to etcd
-my_ds.put_dict(key,dct)
+# first argument is key, second argument is file
+key = sys.argv[1]
+if len(sys.argv) == 3:
+    print(f"Pushing {sys.argv[2]} to {key}...")
+    fl = sys.argv[2]
+
+    # read file
+    with open(fl, 'r') as stream:
+        try:
+            dct = yaml.load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print('cannot open yaml file')
+            exit(1)
+
+    # push to etcd
+    my_ds.put_dict(key, dct)
+
+else:
+    print(f"No file provided. Printing {key} values...")
+    dct = my_ds.get_dict(key)
+    print(dct)
+
+
+
 
 
